5eabd61760bd6af99cc213a9a1df09be59eee790
João Bispo <joaobispo2077@gmail.com>
2021-08-16 15:56:25 -0300

    feat: add header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, GithubLogo, SearchForm } from './styles';
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <GithubLogo />
+      <SearchForm>
+        <input placeholder="Search or jump to..." type="text" />
+      </SearchForm>
+    </Container>
+  );
 };
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,40 @@
 import styled from 'styled-components';
+import { FaGithub } from 'react-icons/fa';
 
-export const Container = styled.header``;
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  background: var(--header);
+
+  padding: 11px 16px;
+`;
+
+export const GithubLogo = styled(FaGithub)`
+  fill: var(--logo);
+  height: 32px;
+  width: 32px;
+  flex-shrink: 0;
+`;
+
+export const SearchForm = styled.form`
+  padding-left: 16px;
+  width: 100%;
+
+  input {
+    background: var(--search);
+    border-radius: 6px;
+    padding: 7px 12px;
+    width: 100%;
+
+    color: white;
+    transition: width 200ms ease-out, color 200ms ease-out;
+
+    &:focus {
+      width: 318px;
+
+      &::placeholder {
+        color: var(--gray);
+      }
+    }
+  }
+`;
