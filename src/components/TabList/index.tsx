5335e92f0c20c36f6bb2fe8190baf9c22f8cc254
João Bispo <joaobispo2077@gmail.com>
2021-08-18 20:32:24 -0300

    feat: compose TabListProps with TabContentProps

--- src/components/TabList/index.tsx
+++ src/components/TabList/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import TabContent from '../TabContent';
+import TabContent, { TabContentProps } from '../TabContent';
 
 import { Container } from './styles';
 
 export type TabListProps = {
   dispositive: 'mobile' | 'desktop';
-};
+} & TabContentProps;
 
-const TabList: React.FC<TabListProps> = ({ dispositive }) => {
+const TabList: React.FC<TabListProps> = ({ dispositive, public_repos }) => {
   return (
     <Container className={dispositive}>
       <div className="wrapper">
         <span className="offset" />
-        <TabContent />
+        <TabContent public_repos={public_repos} />
       </div>
       <span className="line"></span>
     </Container>
