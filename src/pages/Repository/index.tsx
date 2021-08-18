2bfb26ddd70bae5f1aaf143d17e4e47eb7f9ee97
João Bispo <joaobispo2077@gmail.com>
2021-08-18 18:39:42 -0300

    feat: add repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from './styles';
 
 const Repository: React.FC = () => {
-  return <div>Repository</div>;
+  const username = 'joaobispo2077';
+  const reponame = 'happy';
+  const description =
+    'O Happy é uma plataforma que mapeia orfanatos com o intuito de doadores de sonhos e instituições encontrarem-os de forma eficiente.';
+  const stars = 3;
+  const forks = 0;
+
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className="username" to={`/${username}`}>
+          {username}
+        </Link>
+
+        <span>/</span>
+
+        <Link className="reponame" to={`/${username}/${reponame}`}>
+          {reponame}
+        </Link>
+      </Breadcrumb>
+
+      <p>{description}</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>{stars}</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>{forks}</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={`https://github.com/${username}/${reponame}`}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 };
 
 export default Repository;
