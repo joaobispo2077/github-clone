956b7767c8cdc2bde596446ced5996c93fd3b3ee
João Bispo <joaobispo2077@gmail.com>
2021-08-19 14:44:14 -0300

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import 'react-calendar-heatmap/dist/styles.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Profile from './pages/Profile';
 import Repository from './pages/Repository';
 import GlobalStyles from './styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import { ThemeName, themes } from './styles/themes';
 
 function App() {
+  const [themeName, setThemeName] = useState<ThemeName>('dark');
+  const currentTheme = themes[themeName];
+
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Profile />} />
-        <Route path="/:username" element={<Profile />} />
-        <Route path="/:username/:reponame" element={<Repository />} />
-      </Routes>
-      <Footer />
+      <ThemeProvider theme={currentTheme}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/:username" element={<Profile />} />
+          <Route path="/:username/:reponame" element={<Repository />} />
+        </Routes>
+        <Footer />
 
-      <GlobalStyles />
+        <GlobalStyles />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
