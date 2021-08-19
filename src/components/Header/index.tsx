ad66a4c9919c117af8f7c3459317279f39b22080
João Bispo <joaobispo2077@gmail.com>
2021-08-18 22:05:13 -0300

    feat: add handle user submit user profile/repo page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 import { Container, GithubLogo, SearchForm } from './styles';
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.currentTarget.value);
+  };
+
+  const handleSearch = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const hasSearchQuery = searchQuery.length > 0;
+
+    console.log(searchQuery);
+    if (!hasSearchQuery) return;
+    console.info('go to repo or user page');
+
+    return navigate(`/${searchQuery}`);
+  };
+
   return (
     <Container>
       <GithubLogo />
-      <SearchForm>
-        <input placeholder="Search or jump to..." type="text" />
+      <SearchForm onSubmit={handleSearch}>
+        <input
+          placeholder="Search or jump to..."
+          type="text"
+          value={searchQuery}
+          onChange={handleInput}
+        />
       </SearchForm>
     </Container>
   );
